ffd0c17d7f3cb681aa95bf717bcf86aeca1753c5
affan9431 <123025121+affan9431@users.noreply.github.com>
2024-09-05 14:06:24 +0530

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -9,6 +9,13 @@ public class Constants {
     public static final String CONTINUE_PROGRAM = "Do you want to continue? (yes/no)";
     public static final String EXIT_PROGRAM = "Exiting the program...";
     public static final String INVALID_INPUT = "Invalid input! Please enter a valid number.";
+    public static final String OUTPUT_STRING = "Output string: ";
+    public static final String PALINDROME = "Yes, it is palindrome.";
+    public static final String NOT_A_PALINDROME = "No, it is palindrome.";
+    public static final String NUMBER_OF_WORD= "Number of words: ";
+    public static final String REVERSE_STRING = "Reversed string: ";
+    public static final String SORTED_STRING = "Sorted string: ";
+    public static final String YES = "yes";
     public static final String SELECT_OPERATION = """
         Choose a string operation:
         1) Append a string
@@ -23,4 +30,8 @@ public class Constants {
         10) Shift characters in the string
         Enter here: 
     """;
+
+
 }
+
+
